b21ea02b408b75d2d36ba9fe3e938d1d4b9f0b6b
cmeloni <carlo.meloni@abinsula.com>
2023-08-17 15:53:27 +0200

    feat: starting footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,57 @@
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <h1>FOOTER</h1>
+      <div className={styles['footer-left']}>
+        <h1 className={styles['footer-logo']}>LOGO</h1>
+      </div>
+      <div className={styles['footer-right']}>
+        <div className={styles['footer-right-menu']}>
+        <ul className={styles["footer-right-menu-list"]}>
+          <li>
+            <Link className={styles["footer-right-menu-list__item"]} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className={styles["footer-right-menu-list__item"]} to="/about">
+              What About Me
+            </Link>
+          </li>
+          <li>
+            <Link className={styles["footer-right-menu-list__item"]} to="/creations">
+              Creations
+            </Link>
+          </li>
+          <li>
+            <Link className={styles["footer-right-menu-list__item"]} to="/contacts">
+              Let's keep in touch
+            </Link>
+          </li>
+        </ul>
+        </div>
+        <div className={styles['footer-right-menu']}>
+        <ul className={styles["footer-right-menu-list"]}>
+          <li>
+            <a className={styles["footer-right-menu-list__item"]} href="https://www.google.com">
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a className={styles["footer-right-menu-list__item"]} href="https://www.google.com">
+              Crypto
+            </a>
+          </li>
+          <li>
+            <a className={styles["footer-right-menu-list__item"]} href="https://www.google.com">
+              Facebook
+            </a>
+          </li>
+        </ul>
+        </div>
+      </div>
     </div>
   );
 };
