d6f21b945b7e377af2576a0818c3608bfeaf5de0
cmeloni <carlo.meloni@abinsula.com>
2023-09-01 20:16:49 +0200

    feat: dynamically rendering list

--- src/pages/creations/Creations.tsx
+++ src/pages/creations/Creations.tsx
@@ -1,56 +1,50 @@
 import styles from "./Creations.module.scss";
 
+const data = [
+  {
+    title: "Title",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam",
+    image: "https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg",
+  },
+  {
+    title: "Title",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam",
+    image: "https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg",
+  },
+  {
+    title: "Title",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam",
+    image: "https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg",
+  },
+  {
+    title: "Title",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam",
+    image: "https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg",
+  },
+  {
+    title: "Title",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam",
+    image: "https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg",
+  },
+];
+
 function Creations() {
   return (
     <div>
       <h2 className={styles.title}>Creations</h2>
       <div className={styles.list}>
-        <div className={styles["list-item"]}>
-            <img src="https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg" alt="placeholder" />
-            <div className={styles["list-item__content"]}>
-              <h3 className={styles["list-item__title"]}>Title</h3>
-              <p className={styles["list-item__description"]}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              </p>
-            </div>
-        </div>
-        <div className={styles["list-item"]}>
-            <img src="https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg" alt="placeholder" />
-            <div className={styles["list-item__content"]}>
-              <h3 className={styles["list-item__title"]}>Title</h3>
-              <p className={styles["list-item__description"]}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              </p>
-            </div>
-        </div>
-        <div className={styles["list-item"]}>
-            <img src="https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg" alt="placeholder" />
-            <div className={styles["list-item__content"]}>
-              <h3 className={styles["list-item__title"]}>Title</h3>
-              <p className={styles["list-item__description"]}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              </p>
-            </div>
-        </div>
-        <div className={styles["list-item"]}>
-            <img src="https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg" alt="placeholder" />
-            <div className={styles["list-item__content"]}>
-              <h3 className={styles["list-item__title"]}>Title</h3>
-              <p className={styles["list-item__description"]}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              </p>
-            </div>
-        </div>
-        <div className={styles["list-item"]}>
-            <img src="https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg" alt="placeholder" />
-            <div className={styles["list-item__content"]}>
-              <h3 className={styles["list-item__title"]}>Title</h3>
-              <p className={styles["list-item__description"]}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              </p>
-            </div>
-        </div>
+        {data.map((item, index) => (
+          <div key={index} className={styles["list-item"]}>
+          <img src={item.image} alt="placeholder" />
+          <div className={styles["list-item__content"]}>
+            <h3 className={styles["list-item__title"]}>{item.title}</h3>
+            <p className={styles["list-item__description"]}>
+              {item.description}
+            </p>
+          </div>
       </div>
+        ))}
+</div>
     </div>
   );
 }
