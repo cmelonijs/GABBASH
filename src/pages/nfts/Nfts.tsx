55bcf3995f68399da658df93b28d57d1964cc334
cmeloni <carlo.meloni@abinsula.com>
2023-09-24 13:07:11 +0200

    feat: nfts page

--- src/pages/nfts/Nfts.tsx
+++ src/pages/nfts/Nfts.tsx
@@ -1,9 +1,20 @@
-// import styles from "./App.module.scss";
+import { Link } from "react-router-dom";
+import styles from "./Nfts.module.scss";
 
 function Nfts() {
   return (
     <div>
-      <h2>NFTs</h2>
+      <h2 className={styles.title}>NFTs</h2>
+      <div className={styles.container}>
+      <Link className={styles.category} to="/vectors">
+        <img src="https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg" className={styles['category-image']} alt="placeholder" />
+        <h3 className={styles['category-title']}>Vectors</h3>
+      </Link>
+      <Link className={styles.category} to="/concepts">
+        <img src="https://gdrtales.files.wordpress.com/2012/02/angelo-di-platino.jpg" className={styles['category-image']} alt="placeholder" />
+        <h3 className={styles['category-title']}>Concepts</h3>
+      </Link>
+      </div>
     </div>
   );
 }
