ef10d9109d468621c984931bd654ff1232bd2f84
cmeloni <carlo.meloni@abinsula.com>
2023-10-14 19:52:58 +0200

    feat: background detail component image

--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
 import styles from "./Detail.module.scss";
+import { data } from "../../data";
 
 function Detail() {
   const { id } = useParams();
-  console.log(id);
-  return <div>{id}</div>;
+  const creation = data.find((item: any) => item.id === id);
+  const backgroundImage = creation ? creation.image : null;
+  return (
+    <div
+      className={styles.detail}
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      {id}
+    </div>
+  );
 }
 
 export default Detail;
